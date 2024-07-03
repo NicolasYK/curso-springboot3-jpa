4cccd6804daf70b662de741c1911bcd0a99056f2
NicolasYK <kosaka.nicolas@gmail.com>
2024-07-03 18:48:43 -0300

    Service layer, component registration

--- src/main/java/com/cursojava/curso/resources/UserResource.java
+++ src/main/java/com/cursojava/curso/resources/UserResource.java
@@ -1,21 +1,38 @@
 package com.cursojava.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.cursojava.curso.entities.User;
+import com.cursojava.curso.services.UserService;
 
 // Disponibilizando recursos web do usuário.
 @RestController
 @RequestMapping(value = "/users")
 public class UserResource {
 	
-	// Método endpoint para acessar usuário
+	// criando dependência
+	@Autowired
+	private UserService service;
+	
+	// Método endpoint retornando todos os usuários.
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Maria", "maria@gmail.com", "912345678", "12345");
-		return ResponseEntity.ok().body(u);
+	public ResponseEntity<List<User>> findAll(){
+		
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	// Método endpoint que precisa da URL, definindo o valor nele.
+	@GetMapping(value= "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){ // Dizendo para o Spring a val utilizavel.
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
