83f41fdc2c98da5d381d8e77893321d7b404e9ff
NicolasYK <kosaka.nicolas@gmail.com>
2024-07-04 19:17:33 -0300

    Category

--- src/main/java/com/cursojava/curso/config/Test.java
+++ src/main/java/com/cursojava/curso/config/Test.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.cursojava.curso.entities.Category;
 import com.cursojava.curso.entities.Order;
 import com.cursojava.curso.entities.User;
 import com.cursojava.curso.entities.enums.OrderStatus;
+import com.cursojava.curso.repositories.CategoryRepository;
 import com.cursojava.curso.repositories.OrderRepository;
 import com.cursojava.curso.repositories.UserRepository;
 
@@ -26,6 +28,8 @@ public class Test implements CommandLineRunner{
 	@Autowired
 	private OrderRepository orderRepository;
 	
+	@Autowired
+	private CategoryRepository categoryRepository;
 	
 	// Populando o bando de dados.
 	@Override
@@ -38,9 +42,14 @@ public class Test implements CommandLineRunner{
 		Order o2 = new Order(null, Instant.parse("2019-07-21T03:42:10Z"), OrderStatus.WAITING_PAYMENT, u2);
 		Order o3 = new Order(null, Instant.parse("2019-07-22T15:21:22Z"), OrderStatus.SHIPPED, u1);
 		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers");
+		
 		// Salvando os dados chamando o repositório do usuário.
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 	}
 	
 }
